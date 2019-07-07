cf46517f7195ad4b715c0def7f086a14d10f9525
macbook <macbook@mariachmacbook2.lan>
2019-07-08 01:14:23 +0200

    Modello ricorsivo giusto ma non funzionante

--- src/it/polito/tdp/ufo/model/Model.java
+++ src/it/polito/tdp/ufo/model/Model.java
@@ -95,22 +95,7 @@ public class Model {
 			ottima= new ArrayList<>(parziale);
 		}
 		
-		
-//		List<String> candidati = this.getSuccessori(parziale.get(parziale.size()-1));
-//		for(String candidato : candidati) {
-//			if(!parziale.contains(candidato)) {
-//				//è un candidato che non ho ancora considerato
-//				parziale.add(candidato);
-//				this.cercaPercorso(parziale);
-//				parziale.remove(parziale.size()-1);
-//			}
-//		}
-//		
-//		
-//		//vedere se la soluzione corrente è migliore della ottima corrente
-//		if(parziale.size() > ottima.size()) {
-//			this.ottima = new LinkedList(parziale);
-//		}
+	
 	}
 	
 }
